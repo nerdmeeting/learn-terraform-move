e60bf96609d1ac2853ad1d1ed8b301599b87c39d
Nathan Bills <nathan@nerdmeeting.com>
2024-12-29 15:17:05 -0700

    Move the aws_ami.ubuntu and aws_instance.example data and resource to the new compute module; update subnet_id and vpc_security_group_ids to reference the recently-created variables.

--- main.tf
+++ main.tf
@@ -33,39 +33,3 @@ module "security_group" {
   source = "./modules/security_group"
   vpc_id = module.vpc.vpc_id
 }
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "example" {
-  ami                         = data.aws_ami.ubuntu.id
-  subnet_id                   = module.vpc.public_subnets[0]
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-  tags = {
-    Name = "terraform-learn-move-ec2"
-  }
-}
